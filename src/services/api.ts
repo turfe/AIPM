14a9e038b486e9bfcf2d312b2b80a5959a3c68a3
turfe <malevannyi.vm@gmail.com>
2024-12-15 01:34:27 +0100

    added backend url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:4000';
+// const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'https://aipm-qyby.onrender.com';
 
 interface LoginCredentials {
   username: string;

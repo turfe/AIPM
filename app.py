5554228015d6726b789ec459996eab5e867cbd7e
turfe <malevannyi.vm@gmail.com>
2024-12-15 01:36:38 +0100

    added frontend url

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app = Flask(__name__)
 #         "supports_credentials": True
 #     }
 # })
-CORS(app, supports_credentials=True, origins="*")
+CORS(app, supports_credentials=True, origins=['https://polyswipe.netlify.app'])
 
 # Configuration
 app.config["SECRET_KEY"] = os.urandom(32)  # Replace with a secure key
